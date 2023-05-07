a25dc10f623e0f986639efd946038e0b9da24b72
leetleetleetio <130842299+leetleetleetio@users.noreply.github.com>
2023-05-07 19:18:46 -0400

    Update beginner1-comments.py

--- 0001-two-sum/beginner1-comments.py
+++ 0001-two-sum/beginner1-comments.py
@@ -1,14 +1,11 @@
 def twoSum(nums, target):
-    # prevMap stores previously seen differences
-    prevMap = {}
+    # Store differences in a dictionary
 
-    for i, n in enumerate(nums):
-        diff = target - n
+    # Loop through array
+        # Calculate target - current. This is the difference
         
-        # if a difference is found in prevMap, then immediately return result
-        if diff in prevMap:
-            return [prevMap[diff], i]
+        # If a difference is found in prevMap, then
+        # then return result 
         
         # If we reach here, that means that difference was not in prevMap. 
-        # So we can put it in prevMap. If not we have a big problem. 
-        prevMap[n] = i
+        # So we can put it in prevMap.
