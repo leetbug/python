b2ed79a7f17097adc2a2f58b8c4abb640c2cc54e
leetleetleetio <130842299+leetleetleetio@users.noreply.github.com>
2023-05-07 19:21:29 -0400

    Update beginner1.py

--- 0001-two-sum/beginner1.py
+++ 0001-two-sum/beginner1.py
@@ -4,6 +4,8 @@ def twoSum(nums, target):
 
     for i, n in enumerate(nums):
         diff = target - n
+        
         if diff in prevMap:
             return [prevMap[diff], i]
+        
         prevMap[n] = i
