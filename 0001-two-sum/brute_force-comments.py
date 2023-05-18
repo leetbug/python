8f9937144fedcc912e1693fccb17b8783725173f
leetbug <130842299+leetbug@users.noreply.github.com>
2023-05-18 16:34:45 -0400

    Update and rename brute_force-commentstest.py to brute_force-comments.py

--- 0001-two-sum/brute_force-comments.py
+++ 0001-two-sum/brute_force-comments.py
@@ -1,6 +1,6 @@
   
 def twoSum(nums, target):
-    
+    # loop through the list and then another list
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[j] == target - nums[i]:
